322279051fb9d3c626c68813b53103bb5b05ef44
The80sCalled <fischer.mt@gmail.com>
2016-06-07 13:17:17 +0800

    Configuration and logging

---
+++ main.py
@@ -0,0 +1,22 @@
+import base
+import sys
+import os
+import logging
+
+def _prepare_config(config):
+    """
+    Loads the program configuration from the given json file.
+    """
+
+    def expand_config_path(key): config[key] = os.path.expanduser(config[key])
+
+    expand_config_path('aqi_files_path')
+
+if __name__ == "__main__":
+
+    config = base.Init(sys.argv)
+    _prepare_config(config)
+
+    logging.info("Reading AQI files from %s" % config['aqi_files_path'])
+
+
